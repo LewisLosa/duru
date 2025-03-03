9cc9e157a6b159c4a3747c73433acfdddbeeea45
LewisLosa <lewislosa@gmail.com>
2025-03-04 01:05:35 +0300

    ç

---
+++ Duru.UI/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Windows;
+using Duru.Application.Extensions;
+using Duru.Application.Interfaces;
+using Duru.Application.Mappings;
+using Duru.Application.Services;
+using Duru.UI.ViewModels;
+
+namespace Duru.UI
+{
+    public partial class App : System.Windows.Application
+    {
+        public static ServiceProvider? ServiceProvider { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddScoped<MainWindowViewModel>();
+            serviceCollection.AddTransient<Views.MainWindow>();
+            serviceCollection.AddAutoMapper(typeof(RoomProfile));
+            serviceCollection.AddApplicationServices();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var mainWindow = serviceProvider.GetRequiredService<Views.MainWindow>(); 
+            mainWindow.Show();
+        }
+    }
+}
---
+++ Duru.UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+using Duru.UI.ViewModels;
+
+namespace Duru.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow(MainWindowViewModel vm)
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+}
