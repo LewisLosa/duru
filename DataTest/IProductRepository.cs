fb34b8453ab811277ba43b8c6e093288ae7f05c6
Eyüp Şengöz <lewislosa@gmail.com>
2025-04-20 17:46:50 +0300

    Kullanılmayacaklar silindi.

--- DataTest/IProductRepository.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DataTest;
-
-public interface IProductRepository : IRepository<Product>
-{
-    Task<List<Product>> GetProductsByNameAsync(string name, CancellationToken cancellationToken = default);
-}
